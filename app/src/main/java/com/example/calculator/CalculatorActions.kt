faa2aaa3dceaa0c2628cddc8633cd3edbe002fd4
Filip <29filip@gmail.com>
2024-02-19 18:20:08 +0000

    calculator app

--- app/src/main/java/com/example/calculator/CalculatorActions.kt
+++ app/src/main/java/com/example/calculator/CalculatorActions.kt
@@ -1,10 +1,10 @@
 package com.example.calculator
 
 sealed class CalculatorActions {
-    data class Number(val number : Int) : CalculatorActions()
-    object Clear: CalculatorActions()
-    object Delete: CalculatorActions()
-    object Decimal: CalculatorActions()
-    object Calculate: CalculatorActions()
-    data class Operation(val operation: CalculatorOperation): CalculatorActions()
+    data class Number(val number: Int) : CalculatorActions()
+    object Clear : CalculatorActions()
+    object Delete : CalculatorActions()
+    object Decimal : CalculatorActions()
+    object Calculate : CalculatorActions()
+    data class Operation(val operation: CalculatorOperation) : CalculatorActions()
 }
