7a6ae8b194fe0e1f7d5bda0535e50a224b2a12c6
Filip <29filip@gmail.com>
2024-02-19 20:31:53 +0000

    calculator

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -45,6 +45,7 @@ fun Calculator(
                 fontSize = 60.sp,
                 color = Color.White,
                 maxLines = 2
+                // TODO: Make Calculation and Result numbers in text value not overlap on new column when numbers exceed character limit on one line
             )
             Row(
                 modifier = Modifier.fillMaxWidth(),
